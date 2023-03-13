7c1839d9a0b069d649af35b2dd6ecfc8fbbc1eb5
Diptish Banerjee <diptish.banerjee@lcieducation.net>
2023-03-13 17:00:55 -0400

    Added unity project files

---
+++ Torchlight/Assets/Scripts/Enemy.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    // References to components
+    private GameObject player;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    // Enemy properties
+    [SerializeField] private float speed = 1f;
+    [SerializeField] public bool isTrackingPlayer = true;
+    [SerializeField] private float enemyHP = 3f;
+    [SerializeField] private float enemyKillCount = 0f;
+
+    // Start is called before the first frame update
+    protected virtual void Start()
+    {
+        GetComponents();
+        PlayIdleAnimation();
+    }
+
+    // Update is called once per frame
+    protected virtual void Update()
+    {
+        MoveTowardsPlayer();
+        FlipSprite();
+        CheckForPlayer();
+    }
+
+    // Get references to components
+    private void GetComponents()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    // Play the Idle animation
+    private void PlayIdleAnimation()
+    {
+        animator.Play("warrior1_idle");
+    }
+
+    // Move the enemy towards the player
+    private void MoveTowardsPlayer()
+    {
+        Vector3 destination = player.transform.position;
+        Vector3 source = transform.position;
+        Vector3 direction = destination - source;
+
+        if (!isTrackingPlayer)
+        {
+            direction = new Vector3(1, 0, 0);
+        }
+
+        direction.Normalize();
+        transform.position += direction * Time.deltaTime * speed;
+    }
+
+    // Flip the sprite based on the direction
+    private void FlipSprite()
+    {
+        if (isTrackingPlayer)
+        {
+            if (transform.position.x < player.transform.position.x)
+            {
+                spriteRenderer.flipX = false;
+            }
+            else if (transform.position.x > player.transform.position.x)
+            {
+                spriteRenderer.flipX = true;
+            }
+        }
+    }
+
+    // Check if the enemy has found the player and change the animator's state accordingly
+    private void CheckForPlayer()
+    {
+        if (isTrackingPlayer)
+        {
+            animator.Play("warrior1_run");
+        }
+        else
+        {
+            animator.Play("warrior1_walk");
+        }
+    }
+}
---
+++ Torchlight/Assets/Scripts/GameManager.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] GameObject warrior1;
+    [SerializeField] GameObject warrior2;
+    [SerializeField] GameObject warrior3;
+    [SerializeField] GameObject player;
+
+    [SerializeField] int waveoffset = 15;
+    [SerializeField] int hordeoffset = 15;
+
+
+
+
+    void Start()
+    {
+        // Check which level is currently loaded
+        if (SceneManager.GetActiveScene().name == "SampleScene")
+        {
+            // Start spawning enemies using the SpawnEnemy1Coroutine if Level 1 is loaded
+            StartCoroutine(SpawnEnemyWave1Coroutine());
+        }
+       
+    }
+
+
+    public void Update()
+    {
+       
+
+    }
+        
+
+
+
+    private IEnumerator SpawnEnemyWave1Coroutine()
+    {
+        SpawnEnemies(warrior1, 2);
+        yield return new WaitForSeconds(2f);
+        SpawnEnemies(warrior1, 2);
+        yield return new WaitForSeconds(3f);
+        SpawnEnemies(warrior1, 1,false);
+        yield return new WaitForSeconds(3f);
+        SpawnEnemies(warrior1, 5);
+        yield return new WaitForSeconds(4f);
+        SpawnEnemies(warrior1, 3);
+        SpawnEnemies(warrior1, 3);
+        yield return new WaitForSeconds(4f);
+        SpawnEnemies(warrior1, 1, false);
+        SpawnEnemies(warrior1, 1, false);
+        yield return new WaitForSeconds(4f);
+        SpawnEnemies(warrior1, 5);
+        SpawnEnemies(warrior1, 4);
+
+    }
+
+    
+    //The enemies will follow the player when isTracking is true, or they will move to the right when isTracking is false.
+
+    void SpawnEnemies(GameObject enemyPrefab, int numberOfEnemies, bool isTracking = true)
+    {
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            Vector3 spawnPosition;
+
+            // If isTracking is true, set the spawn position to be near the player
+            if (isTracking)
+            {
+                spawnPosition = Random.insideUnitCircle.normalized * waveoffset;
+                spawnPosition += player.transform.position;
+            }
+            // If isTracking is false, set the spawn position to be to the right of the player
+            else
+            {
+                spawnPosition = player.transform.position + Vector3.right * (-hordeoffset);
+            }
+
+            // Instantiate the enemy at the calculated position
+            GameObject enemyobject = Instantiate(enemyPrefab, spawnPosition, Quaternion.identity);
+
+            // If isTracking is false, set the enemy's isTrackingPlayer property to false
+            if (!isTracking)
+            {
+                Enemy enemy = enemyobject.GetComponent<Enemy>();
+                enemy.isTrackingPlayer = false;
+            }
+        }
+    }
+
+
+
+
+
+
+}
+
